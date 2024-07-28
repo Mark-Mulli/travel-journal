c5cdd6b7050ffa92e79c16a99afa41ca3afe0dd1
Mark Mulli <markmulli001@gmail.com>
2024-07-28 09:01:14 +0300

    Navbar Component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Navbar from "./components/Navbar"
+
+import "./styles.css"
+
+export default function App() {
+    //Components Here
+
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+export default function Navbar() {
+
+    return (
+        <nav className="navbar">
+            <i class="ri-earth-line"></i>
+            <span>my travel journal</span>
+    </nav>
+    )
+}
